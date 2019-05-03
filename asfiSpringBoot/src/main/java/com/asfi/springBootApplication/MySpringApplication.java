d93cafc20a5368527e0e81e33dc5df068fd108df
dell <asfiyaoct@gmail.com>
2019-05-03 22:05:12 +0200

    Simple Spring Boot Application setup

---
+++ asfiSpringBoot/src/main/java/com/asfi/springBootApplication/MySpringApplication.java
@@ -0,0 +1,16 @@
+package com.asfi.springBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(MySpringApplication.class, args);
+
+	}
+
+}
